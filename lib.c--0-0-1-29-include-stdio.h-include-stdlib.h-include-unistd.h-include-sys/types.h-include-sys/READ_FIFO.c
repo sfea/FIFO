4b2f2d6e1ee8846ac8cd6d5d5c06eb39d949e913
Fedor <sfeaal@gmail.com>
2014-11-07 21:46:05 +0300

    Update READ_FIFO.c

--- lib.c--0-0-1-29-include-stdio.h-include-stdlib.h-include-unistd.h-include-sys/types.h-include-sys/READ_FIFO.c
+++ lib.c--0-0-1-29-include-stdio.h-include-stdlib.h-include-unistd.h-include-sys/types.h-include-sys/READ_FIFO.c
@@ -4,25 +4,44 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include "mylib.h"
 
-#define FILE "/home-local/student/Загрузки/fifo_file.txt"
+#define FILE "./fifo_file.txt"
 
 int main(int argc, char** argv)
 {
 	int fd = 0;
-	
+	int f = fork();
+	char* buf = (char*)calloc(1024, sizeof(char));
+
+
 	if (argc > 1)
-		printf("INVALID_COUNT_OF_ARGS\n");
+	{
+		perror("ARGUMENTS");
+		exit(1);
+	}
 
 	fd = open(FILE, O_RDONLY);
 
 	if (fd < 0)
-		printf("ERROR_WITH_FIFOFILE_OPENING\n");
+	{
+		perror("FIFO");
+		exit(1);
+	}
 
-	old_new(fd, STDOUT_FILENO);
-	
+	new_read(fd, buf);
+	new_write(buf, STDOUT_FILENO);
 	close(fd);
 	// мы не проходили такой функции - замените на изученные на семинарах
-	unlink(FILE);
+
+	//исправлено
+	if (f = 0)
+	{
+		execlp("rm", "rm", FILE, NULL);
+		perror("EXEC");
+		exit(1);
+	}
+
 	return 0;
 }
+
